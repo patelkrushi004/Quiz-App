686e96e03fcea7dd75aa76e11c752c3cba291f7c
patelkrushi004 <patelkrushi004@gmail.com>
2025-08-16 17:45:16 +0530

    16-08-25

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
+const answerOptions=document.querySelector(".answer-options")
+
 let quizCategory="programming";
 
+//fetch a random question from based on the selected category
 const getRandomQuestion=()=>{
-      const categoryQuestion = questions.find(cat => cat.category === quizCategory)
+      const categoryQuestions = questions.find(cat => cat.category .toLowerCase() === quizCategory.toLowerCase()).questions || [];
+
+      const randomQuestion=categoryQuestions[Math.floor(Math.random()*categoryQuestions.length)];
+      return randomQuestion;
 }
+
+//render the current question and its options in the quiz
+const renderQuestion=()=>{
+  const currentQuestion=getRandomQuestion();
+  if(!currentQuestion) return;
+  console.log(currentQuestion);
+
+  //update the UI
+  answerOptions.innerHTML="";
+  document.querySelector(".questions-text").textContent=currentQuestion.question;
+
+  //create option <li> elements and append them
+  currentQuestion.options.forEach(option=>{
+      const li=document.createElement("li");
+      li.classList.add(".answer-option");
+      li.textContent=option;
+      answerOptions.appendChild(li);
+  });
+}
+
+renderQuestion();
